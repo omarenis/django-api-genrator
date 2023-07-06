7271f7357f87915059db480b9eb249244605ebd0
Omar TRIKI <omartriki@stratisworldwide.com>
2023-07-06 15:15:27 +0100

    adding the package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='django-api-generator',
+      version='0.1',
+      description='django genrator api package for my cms',
+      url='https://github.com/omarenis/django-api-genrator.git',
+      author='Omar Triki',
+      author_email='omartriki712@gmail.com',
+      license='MIT',
+      packages=['django-api-generator'],
+      install_requires=[
+          'django',
+          'djangorestframework',
+          'django-cors-headers'
+      ],
+      zip_safe=False)
